958fd59992b8d05b1f066183b3060e54d487010f
Bhaveshwagh04 <121219011+Bhaveshwagh04@users.noreply.github.com>
2024-10-04 23:13:58 +0530

    mongodb added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Bhavesh",
+    author_email="bhaveshwaghbw.49@gmail.com",
+    packages=find_packages()
+)
